0ef2f414008e2f0ed3a7cb9a478974eaaeda0e88
Felix Lange <felixlange1@gmail.com>
2025-09-22 16:45:31 -0700

    added view models and account controller

---
+++ ExerciseDB/Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+namespace ExerciseDB.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class LoginViewModel
+{
+    [Required, EmailAddress]
+    public string Email { get; set; }
+    
+    [Required, DataType(DataType.Password)]
+    public string Password { get; set; }
+}
